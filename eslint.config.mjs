cb29371c31f9dd242a00c4fab7c5d5954afb1cb2
JGW-Korea <jgw6372@gmail.com>
2025-03-25 18:26:10 +0900

    init: 프로젝트 초기화

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import htmlPlugin from "eslint-plugin-html";
+import cssModulesPlugin from "eslint-plugin-css-modules";
+
+export default [
+  {
+    ignores: ["**/node_modules/**", "**/dist/**"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    files: ["**/*.html"],
+    plugins: {
+      html: htmlPlugin,
+    },
+    rules: {
+      // 필요시 추가 가능
+    },
+  },
+  {
+    files: ["**/*.scss"],
+    plugins: {
+      "css-modules": cssModulesPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "css-modules/no-unused-class": "warn",
+      "css-modules/no-undef-class": "error",
+      "prettier/prettier": "error",
+    },
+  },
+];
