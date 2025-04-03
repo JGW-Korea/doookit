01b4798c55f8b6d6f115fb31dbf348f296414985
JGW-Korea <jgw6372@gmail.com>
2025-04-03 21:21:52 +0900

    refact: favicon 확장자 여러 개 명시

--- src/components/logo/index.ts
+++ src/components/logo/index.ts
@@ -4,6 +4,7 @@ export default class Logo extends Component<ComponentDataType, ComponentDataType
   constructor() {
     super({
       tagName: "a",
+      // props: { ...payload.props },
     });
   }
 
@@ -12,21 +13,35 @@ export default class Logo extends Component<ComponentDataType, ComponentDataType
     this.el.setAttribute("aria-label", "DoooKit 메인 페이지로 이동");
 
     const h1El = document.createElement("h1");
+    const pictureEl = document.createElement("picture");
 
-    const imgEl = document.createElement("img");
-    const imgUrl = new URL("/public/assets/images/favicon.png", import.meta.url);
+    const imgAvifMimiUrl = new URL("/public/assets/images/favicon.avif", import.meta.url);
+    const imgWebpMimiUrl = new URL("/public/assets/images/favicon.webp", import.meta.url);
+    const imgPngMimiUrl = new URL("/public/assets/images/favicon.png", import.meta.url);
 
-    imgEl.src = imgUrl.href;
-    imgEl.classList.add("favicon");
-    imgEl.alt = "favicon";
+    [imgAvifMimiUrl, imgWebpMimiUrl, imgPngMimiUrl].forEach((url, idx, arr) => {
+      const imgEl: HTMLSourceElement | HTMLImageElement =
+        idx !== arr.length - 1 ? document.createElement("source") : document.createElement("img");
 
-    imgEl.width = 32;
-    imgEl.height = 32;
+      if (imgEl instanceof HTMLSourceElement) {
+        imgEl.srcset = url.href;
+        imgEl.type = url.pathname.endsWith("avif") ? "image/avif" : "image/webp";
+      } else {
+        // 새로 만든 img 요소가 HTMLSourceElement가 아닌 HTMLImageElement 인터페이스일 경우
+        imgEl.src = url.href; // 이미지 주소 삽입
+        imgEl.width = 32;
+        imgEl.height = 32;
+        imgEl.alt = "favicon";
+      }
+
+      imgEl.classList.add("favicon"); // 이미지 클래스 삽입
+      pictureEl.appendChild(imgEl);
+    });
 
     const spanEl = document.createElement("span");
     spanEl.textContent = "DoooKit";
 
-    h1El.append(imgEl, spanEl);
+    h1El.append(pictureEl, spanEl);
     this.el.append(h1El);
   }
 }
