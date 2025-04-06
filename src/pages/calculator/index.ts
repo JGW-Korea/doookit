7adccfa6e561052edf56309e59d85d205d4c4c9b
JGW-Korea <jgw6372@gmail.com>
2025-04-06 11:16:34 +0900

    feat: 모바일 환경 계산기 스타일 구성 완료

--- src/pages/calculator/index.ts
+++ src/pages/calculator/index.ts
@@ -1,4 +1,52 @@
+import Swiper from "swiper";
+import { Mousewheel } from "swiper/modules";
+
+import "swiper/swiper-bundle.min.css";
+
 import { Header, Footer } from "../../components";
 
 document.body.insertAdjacentElement("afterbegin", new Header().el);
 document.body.insertAdjacentElement("beforeend", new Footer().el);
+
+Swiper.use([Mousewheel]);
+
+// Swiper Options 구성
+const swiper = new Swiper(".swiper", {
+  loop: false,
+  slidesPerView: 1,
+  direction: "horizontal",
+  allowTouchMove: true,
+  mousewheel: {
+    forceToAxis: true,
+  },
+  on: {
+    slideChange() {
+      if (!(swiperBtn instanceof HTMLElement)) return;
+
+      const buttons = swiperBtn.querySelectorAll("button");
+      buttons.forEach((btn) => {
+        const value = btn.dataset.value;
+        const isBasic = value === "basic" && swiper.activeIndex === 0;
+        const isEngineering = value === "engineering" && swiper.activeIndex === swiper.slides.length - 1;
+
+        btn.classList.toggle("active", isBasic || isEngineering);
+      });
+    },
+  },
+});
+
+// Switch 효과 구현
+const swiperBtn = swiper.el.querySelector(".swiper-btn");
+if (swiperBtn) {
+  swiperBtn.addEventListener("click", (event: Event) => {
+    if (!(event.target instanceof HTMLElement)) return;
+
+    const clickedValue = event.target.dataset.value;
+    if (!clickedValue) return;
+
+    if (clickedValue === "basic" && swiper.activeIndex === 0) swiper.slideTo(swiper.slides.length - 1);
+    else if (clickedValue === "basic" && swiper.activeIndex === swiper.slides.length - 1) swiper.slideTo(0);
+    else if (clickedValue === "engineering" && swiper.activeIndex === swiper.slides.length - 1) swiper.slideTo(0);
+    else swiper.slideTo(swiper.slides.length - 1);
+  });
+}
