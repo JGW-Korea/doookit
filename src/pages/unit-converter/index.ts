f006912a24f0f7246f3b0d386356386b7f3f2a7a
JGW-Korea <jgw6372@gmail.com>
2025-04-13 03:25:25 +0900

    feat: 탭 네비게이션 클릭 전환 기능 추가

--- src/pages/unit-converter/index.ts
+++ src/pages/unit-converter/index.ts
@@ -3,12 +3,41 @@ import { Header, Footer } from "../../components";
 document.body.insertAdjacentElement("afterbegin", new Header().el);
 document.body.insertAdjacentElement("beforeend", new Footer().el);
 
-// 탭 네비게이션 방향키 전환
 const tabList: HTMLElement | null = document.querySelector("nav[role='tablist']");
 const tabs: NodeList | null = document.querySelectorAll("button[role='tab']");
 
 // Tab List, Tabs 요소가 있을 경우에만 실행하는 타입 가드(Type Guard)
 if (tabList && tabs) {
+  let testCurrentTabEl: HTMLElement;
+  tabs.forEach((tabEl) => {
+    if (tabEl instanceof HTMLElement && tabEl.classList.contains("active")) testCurrentTabEl = tabEl;
+  });
+
+  tabs.forEach((tabEl) => {
+    // 각 탭 클릭 이벤트 핸들러 등록
+    tabEl.addEventListener("click", (event) => {
+      if (event.currentTarget instanceof HTMLElement) {
+        // 1. 현재 선택된 요소가 클릭 되었을 경우 -> 무시
+        if (testCurrentTabEl === event.currentTarget) return;
+        // 2. 다른 탭을 선택했을 경우 -> 해당 탭으로 전환
+        else {
+          if (testCurrentTabEl) {
+            testCurrentTabEl.ariaSelected = "false";
+            testCurrentTabEl.tabIndex = -1;
+            testCurrentTabEl.classList.remove("active");
+
+            event.currentTarget.ariaSelected = "true";
+            event.currentTarget.tabIndex = 0;
+            event.currentTarget.classList.add("active");
+
+            testCurrentTabEl = event.currentTarget;
+          }
+        }
+      }
+    });
+  });
+
+  // 탭 네비게이션 방향키 전환
   tabList.addEventListener("keydown", (event) => {
     const activeTab = document.activeElement;
 
